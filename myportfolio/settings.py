9f4cb4eb3a82f6062401328a1836d41992eff2ce
Milan Bhasima <milanbhasima1@gmail.com>
2026-05-28 21:02:51 +0545

    added env variables

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -13,6 +13,10 @@ import os
 import dj_database_url
 from pathlib import Path
 
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
+DEBUG = os.environ.get('DEBUG') == 'True'
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
